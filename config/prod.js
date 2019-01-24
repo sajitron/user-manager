1f40822bf5cc2a496688351fa897dfd0b9b4fd2f
sajicode <sajioloye@gmail.com>
2019-01-24 02:48:50 +0100

    setup image upload route

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,8 @@ module.exports = {
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	mongoURI: process.env.MONGO_URI,
 	cookieKey: process.env.COOKIE_KEY,
-	redisUrl: 'redis://127.0.0.1:6379'
+	redisUrl: 'redis://127.0.0.1:6379',
+	cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+	cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
+	cloud_name: process.env.CLOUD_NAME
 };
---
+++ routes/uploadRoute.js
@@ -0,0 +1,30 @@
+const cloudinary = require('cloudinary');
+const multer = require('multer');
+const cloudinaryStorage = require('multer-storage-cloudinary');
+const keys = require('../config/keys');
+
+cloudinary.config({
+	cloud_name: keys.cloud_name,
+	api_key: keys.cloudinary_api_key,
+	api_secret: keys.cloudinary_api_secret
+});
+
+const storage = cloudinaryStorage({
+	cloudinary,
+	folder: 'user-manager',
+	allowedFormats: [ 'jpg', 'jpeg', 'png' ],
+	transformation: [ { width: 500, height: 500, crop: 'limit' } ]
+});
+
+const parser = multer({ storage });
+
+module.exports = (app) => {
+	app.post('/api/upload', parser.single('image'), (req, res) => {
+		console.log(req.file);
+		const image = {};
+		image.url = req.file.url;
+		image.id = req.file.public_id;
+
+		res.send(image);
+	});
+};
