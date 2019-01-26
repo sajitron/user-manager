272e053a07435b05152adced23e4887a9e4c0485
sajicode <sajioloye@gmail.com>
2019-01-26 22:40:04 +0100

    complete groupField component

--- client/src/components/groups/GroupField.js
+++ client/src/components/groups/GroupField.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const GroupField = () => {
-	return <div>Hello There</div>;
+const GroupField = ({ input, label, meta: { error, touched } }) => {
+	return (
+		<div>
+			<label>{label}</label>
+			<input {...input} />
+			<div>{touched && error}</div>
+		</div>
+	);
 };
 
 export default GroupField;
