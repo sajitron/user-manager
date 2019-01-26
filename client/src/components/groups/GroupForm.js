971e3aea2e8095d37d8ac356ee468acf4fb87c0f
sajicode <sajioloye@gmail.com>
2019-01-26 22:13:17 +0100

    setup GroupForm component

---
+++ client/src/components/groups/GroupForm.js
@@ -0,0 +1,44 @@
+import _ from 'lodash';
+import React, { PureComponent } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import GroupField from './GroupField';
+import formFields from './formFields';
+
+export class GroupForm extends PureComponent {
+	renderFields() {
+		return _.map(formFields, ({ label, name }) => {
+			return <Field key={name} component={GroupField} type="text" label={label} name={name} />;
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.props.handleSubmit(this.props.onGroupSubmit)}>
+					{this.renderFields()}
+					<Link to="/dashboard">Cancel</Link>
+					<button type="submit">Next</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+function validate(values) {
+	const errors = {};
+
+	_.each(formFields, ({ name }) => {
+		if (!values[name]) {
+			errors[name] = 'You need to provide a value';
+		}
+	});
+
+	return errors;
+}
+
+export default reduxForm({
+	validate,
+	form: 'groupForm',
+	destroyOnUnmount: false
+})(GroupForm);
