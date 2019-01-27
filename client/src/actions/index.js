a7753b8bd90a7bfc67540ec9fb303b079d748b63
sajicode <sajioloye@gmail.com>
2019-01-27 20:35:21 +0100

    create fetchGroups action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_GROUPS } from './types';
 
 export const fetchUser = () => async (dispatch) => {
 	const res = await axios.get('/api/current_user');
@@ -11,4 +11,10 @@ export const submitGroup = (values, history) => async (dispatch) => {
 	const res = await axios.post('/api/groups', values);
 	history.push('/dashboard');
 	dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchGroups = () => async (dispatch) => {
+	const res = await axios.get('/api/groups');
+
+	dispatch({ type: FETCH_GROUPS, payload: res.data });
 };
