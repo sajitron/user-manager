ccd07f67cd6cc4f000629f517295509e4e71f6d3
sajicode <sajioloye@gmail.com>
2019-01-29 12:48:50 +0100

    setup memberForm component

---
+++ client/src/components/members/MemberForm.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import MemberField from './MemberField';
+import validateEmails from '../../utils/validateEmails';
+import formFields from './formFields';
+
+export class MemberForm extends Component {
+	renderFields() {
+		return _.map(formFields, ({ label, name }) => {
+			return <Field key={name} component={MemberField} type="text" label={label} name={name} />;
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.props.handleSubmit(this.props.onMemberSubmit)}>
+					{this.renderFields()}
+					<Link to="/dashboard">Cancel</Link>
+					<button type="submit">Next</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+function validate(values) {
+	const errors = {};
+
+	errors.email = validateEmails(values.email || '');
+
+	_.each(formFields, ({ name }) => {
+		if (!values[name]) {
+			errors[name] = 'You need to provide a value';
+		}
+	});
+
+	return errors;
+}
+
+export default reduxForm({
+	validate,
+	form: 'memberForm',
+	destroyOnUnmount: false
+})(MemberForm);
