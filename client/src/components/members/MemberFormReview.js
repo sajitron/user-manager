00ce818ae85ce0de38a2799cd7a3d41ed8e09a74
sajicode <sajioloye@gmail.com>
2019-01-29 13:18:48 +0100

    setup MemberFormReview Component

---
+++ client/src/components/members/MemberFormReview.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import _ from 'lodash';
+import { connect } from 'react-redux';
+// @TODO import withRouter
+import formFields from './formFields';
+import * as actions from '../../actions';
+
+const MemberFormReview = ({ onCancel, formValues, submitMember, history }) => {
+	const reviewFields = _.map(formFields, ({ name, label }) => {
+		return (
+			<div key={name}>
+				<label>{label}</label>
+				<div>{formValues[name]}</div>
+			</div>
+		);
+	});
+
+	return (
+		<div>
+			<h4>Please Confirm Your Entries</h4>
+			{reviewFields}
+			<button onClick={onCancel}>Back</button>
+			<button onClick={() => submitMember(formValues, history)}>Next</button>
+		</div>
+	);
+};
+
+function mapStateToProps(state) {
+	return {
+		formValues: state.form.memberForm.values
+	};
+}
+
+export default connect(mapStateToProps, actions)(MemberFormReview);
