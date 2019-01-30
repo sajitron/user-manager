4bfa496ca1448cdb3055a945a60e13ad8ce24612
sajicode <sajioloye@gmail.com>
2019-01-31 00:26:16 +0100

    create DatePicker component

---
+++ client/src/components/members/DatePicker.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import moment from 'moment';
+import 'react-dates/initialize';
+import { SingleDatePicker } from 'react-dates';
+
+class DatePicker extends PureComponent {
+	render() {
+		return <div />;
+	}
+}
+
+export default DatePicker;
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'react-dates/lib/css/_datepicker.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
