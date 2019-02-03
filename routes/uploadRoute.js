1ecaf0e1dd242857990126078e8eacec1a1167a5
sajicode <sajioloye@gmail.com>
2019-02-03 22:04:27 +0100

    setup upload route for image

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -1,31 +1,17 @@
-const cloudinary = require('cloudinary');
-const multer = require('multer');
-const cloudinaryStorage = require('multer-storage-cloudinary');
-const keys = require('../config/keys');
-const { requireLogin } = require('../middlewares');
-
-cloudinary.config({
-	cloud_name: keys.cloud_name,
-	api_key: keys.cloudinary_api_key,
-	api_secret: keys.cloudinary_api_secret
-});
-
-const storage = cloudinaryStorage({
-	cloudinary,
-	folder: 'user-manager',
-	allowedFormats: [ 'jpg', 'jpeg', 'png' ],
-	transformation: [ { width: 500, height: 500, crop: 'limit' } ]
-});
-
-const parser = multer({ storage });
+const { requireLogin, cloudinaryConfig } = require('../middlewares');
+const { uploads, dataUri, uploader } = cloudinaryConfig;
 
 module.exports = (app) => {
-	app.post('/api/upload', requireLogin, (req, res) => {
-		console.log(req.files);
-		// const image = {};
-		// image.url = req.file.url;
-		// image.id = req.file.public_id;
+	app.post('/api/upload', requireLogin, uploads, (req, res) => {
+		if (req.files.file) {
+			const file = dataUri(req).content;
 
-		res.send('hello');
+			return uploader
+				.upload(file)
+				.then((result) => {
+					return res.status(200).send(result);
+				})
+				.catch((err) => res.status(400).send(err));
+		}
 	});
 };
