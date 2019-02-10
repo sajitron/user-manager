75e21b7af823255771fc72f85f10516cebbd70a8
sajicode <sajioloye@gmail.com>
2019-02-10 20:51:20 +0100

    display images for each profile

--- client/src/components/members/MemberList.js
+++ client/src/components/members/MemberList.js
@@ -1,18 +1,28 @@
 import React, { PureComponent, Fragment } from 'react';
 import { connect } from 'react-redux';
+import { Image } from 'cloudinary-react';
 import { fetchMembers } from '../../actions';
 import moment from 'moment';
 
 class MemberList extends PureComponent {
 	componentDidMount() {
 		this.props.fetchMembers();
+		console.log(process.env.CLOUDINARY_CLOUD_NAME);
 	}
 
 	renderMembers() {
-		return this.props.members.reverse().map(({ _id, firstName, lastName, email, birthDate }) => {
+		return this.props.members.reverse().map(({ _id, firstName, lastName, email, birthDate, imageUrl }) => {
 			return (
 				<Fragment key={_id}>
 					<div>
+						<Fragment>
+							<Image
+								cloudName={process.env.REACT_APP_CLOUD_NAME}
+								publicId={imageUrl}
+								width="100"
+								crop="scale"
+							/>
+						</Fragment>
 						<h4>
 							{firstName} {lastName}
 						</h4>
