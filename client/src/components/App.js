8210af44207ca8efd3130dcce628173c9dcff270
sajicode <sajioloye@gmail.com>
2019-02-13 02:47:05 +0100

    integrate details components into App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,6 +8,8 @@ import Landing from './Landing';
 import Dashboard from './Dashboard';
 import GroupNew from './groups/GroupNew';
 import MemberNew from './members/MemberNew';
+import GroupDetails from './groups/GroupDetails';
+import MemberDetails from './members/MemberDetails';
 import PageNotFound from './PageNotFound';
 
 class App extends PureComponent {
@@ -26,6 +28,8 @@ class App extends PureComponent {
 							<Route path="/dashboard" component={Dashboard} />
 							<Route path="/groups/new" component={GroupNew} />
 							<Route path="/members/new" component={MemberNew} />
+							<Route path="/groups/:id" component={GroupDetails} />
+							<Route path="/members/:id" component={MemberDetails} />
 							<Route component={PageNotFound} />
 						</Switch>
 					</div>
